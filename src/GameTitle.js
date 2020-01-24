43a01474fde7a80a11bd6fa474a1cd3d1bd196dd
ibryanUW <ibryan@uw.edu>
2020-01-24 15:16:47 -0800

    GameTitle file added. ttt_app.js file removed.
    Game title changed and updated.

---
+++ src/GameTitle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GameTitle(props){
+    return (
+        <header id="gameTitle">
+            <h3>Tic Tac Toe</h3>
+        </header>
+    );
+}
